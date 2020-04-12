b1e87f8e1f27b858cfd8a410ae20100d545c1815
mohanrajkpm <infomohanrajsoft@gmail.com>
2020-04-12 13:46:25 +0530

    Add glucose level form and validations

--- app/controllers/glucose_levels_controller.rb
+++ app/controllers/glucose_levels_controller.rb
@@ -3,4 +3,21 @@ class GlucoseLevelsController < ApplicationController
 	def new
 		@glucose_level = GlucoseLevel.new
 	end
+
+	def create
+		@glucose_levels = GlucoseLevel.new(glucose_level_params)
+		if @glucose_levels.save
+			flash[:notice] = t('common.created_successfully', model_name: "Glucose level")
+			redirect_to new_glucose_level_path
+		else
+			flash[:errors] = @glucose_levels.errors.full_messages
+	  redirect_to new_glucose_level_path
+		end
+	end
+
+	private
+		# permit the glucose level params
+    def glucose_level_params
+  		params.require(:glucose_level).permit(:glucose).merge(user_id: current_user.id)
+		end
 end
--- app/models/glucose_level.rb
+++ app/models/glucose_level.rb
@@ -1,2 +1,15 @@
 class GlucoseLevel < ApplicationRecord
+	validates :glucose, presence: true, numericality: { only_integer: true }
+	validates_uniqueness_of :glucose, scope: [:user_id], conditions: -> { where("DATE(created_at) = ?", Date.today) }
+	validate :check_limitation_of_user, on: :create
+	
+	belongs_to :user
+
+	def check_limitation_of_user
+	  errors.add(:glucose, I18n.t("glucose_levels.validations.limit_msg")) if self.number_of_entries == 4
+	end
+
+	def number_of_entries
+		self.class.name.constantize.where("DATE(created_at) = ? AND user_id = ?", Date.today, self.user_id).count if self.glucose.present?
+	end
 end
