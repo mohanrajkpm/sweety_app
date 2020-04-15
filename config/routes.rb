01742798dd79950e8b8f83e0c84d4476291efbaa
mohanrajkpm <infomohanrajsoft@gmail.com>
2020-04-15 21:37:41 +0530

    Change in route path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :glucose_levels
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: "glucose_levels#new"
-  root 'welcome#index'
+  root to: "glucose_levels#new"
+  #root 'welcome#index'
   get 'unauthorized', to: 'home#index'
 end
